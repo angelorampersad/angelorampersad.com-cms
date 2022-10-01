742ab0101ee4509c9eac490257713ad22768f47f
Angelo Rampersad <angelo.rampersad@justeattakeaway.com>
2022-10-01 11:24:53 +0200

    init

---
+++ schemas/schema.js
@@ -0,0 +1,15 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import animal from './animal';
+import post from './post';
+import snippet from './snippet';
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([
+		animal,
+    post,
+    // snippet
+  ]),
+});
